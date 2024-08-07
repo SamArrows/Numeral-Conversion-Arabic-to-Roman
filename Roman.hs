7ff31a7934d5b0e9ff7436845c24d4ce251a58b5
Arrows <craybould700@gmail.com>
2024-08-07 14:44:40 +0100

    More debugging

--- Roman.hs
+++ Roman.hs
@@ -5,6 +5,13 @@ Code to convert an integer to a Roman numeral
 -Only powers of 10 (I, X, C, M) can be appended consecutively at most 3 times to represent multiples of 10. You cannot append 5 (V), 50 (L), or 500 (D) multiple times. If you need to append a symbol 4 times use the subtractive form.
 -}
     
+{-
+Code to convert an integer to a Roman numeral
+-If the value does not start with 4 or 9, select the symbol of the maximal value that can be subtracted from the input, append that symbol to the result, subtract its value, and convert the remainder to a Roman numeral.
+-If the value starts with 4 or 9 use the subtractive form representing one symbol subtracted from the following symbol, for example, 4 is 1 (I) less than 5 (V): IV and 9 is 1 (I) less than 10 (X): IX. Only the following subtractive forms are used: 4 (IV), 9 (IX), 40 (XL), 90 (XC), 400 (CD) and 900 (CM).
+-Only powers of 10 (I, X, C, M) can be appended consecutively at most 3 times to represent multiples of 10. You cannot append 5 (V), 50 (L), or 500 (D) multiple times. If you need to append a symbol 4 times use the subtractive form.
+-}
+    
 convFour :: Int -> String
 convFour x
     | x < 4000 = (multiplyChar (x `div` 1000) "M") ++ convertThreeDigs (x `mod` 1000)
@@ -13,7 +20,7 @@ convFour x
 convertThreeDigs :: Int -> String
 convertThreeDigs x
     | x < 400 = (multiplyChar (x `div` 100) "C") ++ convertTwoDigs (x `mod` 100)
-    | x < 500 = "CD" ++ convertTwoDigs (x `mod` 100)
+    | x < 500 = "CD" ++ convertThreeDigs (x `mod` 100)
     | x < 900 = "D" ++ convertThreeDigs (x - 500)
     | x < 1000 = "CM" ++ convertThreeDigs(x - 900)
     | otherwise = "0"
@@ -21,9 +28,9 @@ convertThreeDigs x
 convertTwoDigs :: Int -> String
 convertTwoDigs x
     | x < 40 = (multiplyChar (x `div` 10) "X") ++ convertDigit (x `mod` 10)
-    | x < 50 = "XL" ++ convertDigit (x `mod` 10)
-    | x < 90 = "L" ++ convertDigit (x - 50)
-    | x < 100 = "XC" ++ convertDigit (x - 90)
+    | x < 50 = "XL" ++ convertTwoDigs (x `mod` 10)
+    | x < 90 = "L" ++ convertTwoDigs (x - 50)
+    | x < 100 = "XC" ++ convertTwoDigs (x - 90)
     | otherwise = "0"
     
 convertDigit :: Int -> String
@@ -42,14 +49,7 @@ multiplyChar x y
 
 main :: IO()
 main =
-    print(convFour 60)
-    
-    
-    
-      
-    
-    
-    
+    print(convFour 900)
     
     
     
